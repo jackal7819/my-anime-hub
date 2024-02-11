322910524382c5127840686646d0d0e4a4af7ab5
Viktor Filippov <jackal3166@gmail.com>
2024-02-11 23:50:38 +0200

    Create App

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+	import { ref, computed, onMounted } from 'vue';
+
+	const query = ref('');
+	const myAnime = ref([]);
+	const searchResults = ref([]);
+
+	const myAnimeAsc = computed(() => {
+		return myAnime.value.sort((a, b) => {
+			return a.title.localeCompare(b.title);
+		});
+	});
+
+	const searchAnime = async () => {
+		try {
+			const response = await fetch(
+				`https://api.jikan.moe/v4/anime?q=${query.value}`
+			);
+			const { data } = await response.json();
+			searchResults.value = data;
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleInput = event => {
+		if (!event.target.value) {
+			searchResults.value = [];
+		}
+	}
+
+	const addAnime = anime => {
+		searchResults.value = [];
+		query.value = '';
+		
+		myAnime.value.push({
+			id: anime.mal_id,
+			title: anime.title,
+			image: anime.images.jpg.image_url,
+			total_episodes: anime.episodes,
+			watchedEpisodes: 0,
+		});
+	};
+
+	localStorage.setItem('myAnime', JSON.stringify(myAnime.value));
+
+	const increaseWatch = anime =>{
+		
+	}
+</script>
+
+<template>
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
